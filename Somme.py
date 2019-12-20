b5b2649618fa45f98cc12f56a530142c921296a0
fama10 <fallfama2@gmail.com>
2019-12-20 13:54:12 +0100

    Modif de Somme

--- Somme.py
+++ Somme.py
@@ -6,17 +6,17 @@ from division import *
 
 a= input("Entrer la valeur de a : ")
 b= input("Entrer la valeur de b : ")
-i= input("Taper la valeur -1, -2, 3, 1 : ")
+i= input("Taper le signe de l'opération : ")
 a = int(a)
 b = int(b)
-i = int(i)
+
 # test de la fonction calcul
-if(i==-1):
-    addition(a, b)
-if(i==-2):
-    soustraction(a,b)
-if(i==3):
-    multiplication(a*b)
-if(i==1):
-    division(a,b)
-os.system("pause")
+if(i== "+"):
+    resultat = addition(a, b)
+if(i=="-"):
+    resultat = soustraction(a,b)
+if(i=="*"):
+    resultat = multiplication(a*b)
+if(i=="/"):
+    resultat = division(a,b)
+print("le resultat de l'operation est :", resultat)
